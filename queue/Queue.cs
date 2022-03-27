9e5b02d4a7a286a310bd2ec039251b8a4fd4533d
Alexander <sbarabas176@gmail.com>
2022-03-28 01:17:35 +0300

    v4

--- queue/Queue.cs
+++ queue/Queue.cs
@@ -1,9 +1,33 @@
 ﻿namespace queue;
 
-public class Queue
+public class Queue<T>
 {
-    public static void Hello()
+    // private int size = 0;
+    // private int last = 0;
+
+    private T[] myArray = new T[0];
+
+    public void Add(T value)
     {
-        Console.WriteLine("Hello");
+        var size = myArray.Length + 1;
+        Array.Resize<T>(ref myArray, size);
+        myArray[size - 1] = value;
+    }
+
+    public T GetElement()
+    {
+        if (myArray.Length == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        T value = myArray[0];
+        myArray = myArray.Skip(1).ToArray();  
+        return value;
+    }
+
+    public int Length()
+    {
+        return myArray.Length;
     }
 }
